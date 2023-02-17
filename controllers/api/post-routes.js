21b8d8a814ab221d838f938bd16f43ebbd57f561
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2023-02-17 15:25:48 -0500

    add routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,17 +1,50 @@
-const router = require('express').Router();
-const { Post } = require('../../models/');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Post } = require("../../models/");
+const withAuth = require("../../utils/auth");
 
-router.post('/', withAuth, async (req, res) => {
-  
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const post = await Post.create({
+      title: req.body.title,
+      body: req.body.body,
+    });
+    if (!post) {
+      res.status(404).json({ message: "can not create" });
+    }
+    res.status(200).res.json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.put('/:id', withAuth, async (req, res) => {
-  
-});
-
-router.delete('/:id', withAuth, async (req, res) => {
-
-});
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const post = Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!post) {
+      res.status(404).json({ message: "no id found" });
+    }
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}),
+  router.delete("/:id", withAuth, async (req, res) => {
+    try {
+      const post = Post.destroy(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!post) {
+        res.status(404).json({ message: "no id found" });
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,16 +1,55 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
 
-router.post('/', async (req, res) => {
-  
+router.post("/", async (req, res) => {
+  try {
+    const userInfo = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userInfo.id;
+      req.session.logged_in = true;
+      res.status(200).json(userInfo);
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.post('/login', async (req, res) => {
-  
+router.post("/login", async (req, res) => {
+  try {
+    const userInfo = await User.findOne({
+      where: { username: req.body.username },
+    });
+
+    if (!userInfo) {
+      res.status(400).json({ message: "incorrect email or password" });
+      return;
+    }
+
+    const passwordVal = await userInfo.checkPassword(req.body.password);
+
+    if (!passwordVal) {
+      res.status(400).json({ message: "incorrect email or password" });
+      return;
+    }
+    req.session.save(() => {
+      (req.session.user_id = userInfo.id),
+        (req.session.logged_in = true),
+        res.json({ user: userInfo, message: "you are now logged in" });
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.post('/logout', (req, res) => {
-  
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
