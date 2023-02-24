2e63889d4503698187496ee5369181696b77fc45
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2023-02-23 23:08:19 -0500

    fix delete

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,36 +1,36 @@
 const id = document.querySelector('input[name="post-id"]').value;
 //sets up edit route for post
-const editFormHandler = async function(event) {
+const editFormHandler = async function (event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
   const body = document.querySelector('textarea[name="post-body"]').value;
 
   await fetch(`/api/post/${id}`, {
-    method: 'PUT',
+    method: "PUT",
     body: JSON.stringify({
       title,
-      body
+      body,
     }),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   });
 
-  document.location.replace('/dashboard');
+  document.location.replace("/dashboard");
 };
 
-const deleteClickHandler = async function() {
-  await fetch(`/api/post/${postId}`, {
-    method: 'DELETE'
+const deleteClickHandler = async function () {
+  await fetch(`/api/post/${id}`, {
+    method: "DELETE",
   });
 
-  document.location.replace('/dashboard');
+  document.location.replace("/dashboard");
 };
 
 document
-  .querySelector('#edit-post-form')
-  .addEventListener('submit', editFormHandler);
+  .querySelector("#edit-post-form")
+  .addEventListener("submit", editFormHandler);
 document
-  .querySelector('#delete-btn')
-  .addEventListener('click', deleteClickHandler);
+  .querySelector("#delete-btn")
+  .addEventListener("click", deleteClickHandler);
